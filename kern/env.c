302aa97c8ce440764452d9a30403d771037eb658
Pezeter <patrik.debnar@hotmail.com>
2018-11-10 20:10:02 +0100

    lab3 - part 1 fixes 2

--- kern/env.c
+++ kern/env.c
@@ -364,7 +364,7 @@ load_icode(struct Env *e, uint8_t *binary)
 
 	for (; ph < eph; ++ph) {
 		if(ph->p_type == ELF_PROG_LOAD) {
-			region_alloc(e, ph->p_va, ph->p_memsz);
+			region_alloc(e, (void*) ph->p_va, ph->p_memsz);
 
 			memset((void*) ph->p_va, 0, ph->p_memsz);
 			memcpy((void*) ph->p_va, binary + ph->p_offset, ph->p_filesz);
